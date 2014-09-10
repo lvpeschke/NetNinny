7903f97fd01384163e0e12f3f07e385fe2b3c3f7
martin <martin@derpian.derpian>
2014-09-10 11:34:27 +0200

    Main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <cstring>
+#include <unistd.h>
+#include <pthread.h>
+
+using namespace std;
+
+#include "exceptions.h"
+#include "tcp_layer.h"
+
+int main ( int argc, char * argv [] )
+ {
+ 	int   socket;
+ 	char* port;
+ 	if ( argc != 2 )
+ 	{
+ 		cout << "Incorrect number of arguments. Arguments should be in following format: ./assignment_2 port_number" << endl;
+ 		exit ( EXIT_FAILURE );
+ 	}
+ 	port = argv[1];
+ 	
+ 	cout << "SERVER RUNNING ON PORT: " << port       << endl;
+ 	
+ 	try 
+ 	{ 
+ 		socket = openServerSocket ( port );
+ 	}
+ 	catch ( CSocketException e )
+ 	{
+ 		cout << e << endl;
+ 		exit ( EXIT_FAILURE );
+ 	}
+ 	serverMain ( socket );
+ 	close ( socket );
+ }

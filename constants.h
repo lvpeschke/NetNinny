d37e3739448a8548c1507f0f5337fe1aa52be28d
Lena <vicky.peschke@gmail.com>
2014-09-21 15:48:19 +0200

    New HTTP headers for redirection
    To be tested

--- constants.h
+++ constants.h
@@ -3,39 +3,46 @@
 
 #define BUFFERSIZE 1000
 
-#define BAD_CONTENT_HEADER "HTTP/1.1 302 Found\r\n Location: http://www.ida.liu.se/~TDTS04/labs/2011/ass2/error2.html\r\n \rn"
-//Header used in the HTTP response in case of bad content
+// Header used in the HTTP response in case of bad url.
+#deine BAD_URL_HEADER "HTTP/1.1 302 Found\r\n Location: http://www.ida.liu.se/~TDTS04/labs/2011/ass2/error2.html\r\n \rn"
 
+// Content used in the HTTP response in case of bad url.
+#define BAD_URL_CONTENT ""
+
+// Header used in the HTTP response in case of bad content.
+#define BAD_CONTENT_HEADER "HTTP/1.1 302 Found\r\n Location: http://www.ida.liu.se/~TDTS04/labs/2011/ass2/error1.html\r\n \rn"
+
+// Content used in the HTTP response in case of bad content.
 #define BAD_CONTENT_CONTENT ""
-//Content used in the HTTP response in case of bad content
 
+// Error that occurs when the host name is NULL.
 #define NO_HOST "Host name is empty"
-//Error that occurs when the host name is NULL.
 
+// Error that occurs when socket cannot be created.
 #define SOCK_ERR "Cannot create socket"
-//Error that occurs when socket cannot be created.
 
+// Error that occurs when socket cannot be bound to port.
 #define BIND_ERR "Cannot bind port to socket" 
-//Error that occurs when socket cannot be bound to port.
 
+// Error that occurs when listening on socket fails.
 #define LIST_ERR "Cannot set socket to listening state" 
-//Error that occurs when listening on socket fails.
 
+// Error that occurs when sending data fails.
 #define SEND_ERR "Cannot send data through socket"
 
+// Error that occurs when socket cannot receive data.
 #define RECV_ERR "Cannot receive data from the socket" 
-//Error that occurs when socket cannot receive data.
 
+// Error that occurs when accept function fails.
 #define ACCE_ERR "Cannot accept connection" 
-//Error that occurs when accept function fails.
 
+// Error that occurs when getaddrinfo function fails.
 #define ADDR_ERR "Error with initial socket setting" 
-//Error that occurs when getaddrinfo function fails.
 
+// Error that occurs when connect function fails.
 #define CONNECTION_FAIL "Connection to socket failed"
-//Error that occurs when connect function fails.
 
-#define CONNECTION_CLOSED "Connection closed by the other side" 
-//Message used when proxy receives data of zero length, signing end of connection.
+// Message used when proxy receives data of zero length, signing end of connection.
+#define CONNECTION_CLOSED "Connection closed by the other side"
 
 #endif
